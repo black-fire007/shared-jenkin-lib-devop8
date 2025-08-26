30b756985b2e608347f3d3a32e0840bc4c40310e
black-fire007 <seth99991111@gmail.com>
2025-08-26 22:58:27 +0700

    feature: add new item to share library

---
+++ vars/pushDockerTODH.groovy
@@ -0,0 +1,8 @@
+del call(String imageName){
+    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+              sh """
+                    echo '$PASSWORD' |  docker login -u $USERNAME --password-stdin   
+                docker push ${imageName}:${env.BUILD_NUMBER} 
+                """
+                    }
+}
