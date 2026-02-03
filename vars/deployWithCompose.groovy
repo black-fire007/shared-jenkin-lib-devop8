620baaafce3cb63630c61fd2b449bd4ca11b73e1
black-fire007 <seth99991111@gmail.com>
2026-02-03 14:55:51 +0700

    add code

---
+++ vars/deployWithCompose.groovy
@@ -0,0 +1,12 @@
+def call(String appType) {
+
+    echo "Deploying using docker-compose for ${appType}"
+
+    def composeContent = libraryResource("${appType}/docker-compose.yml")
+
+    writeFile file: 'docker-compose.yml', text: composeContent
+
+    sh """
+        docker-compose up -d
+    """
+}
