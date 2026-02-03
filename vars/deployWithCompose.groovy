fad447159a0eda4f05332dde926b1bd404b833cc
black-fire007 <seth99991111@gmail.com>
2026-02-03 21:14:41 +0700

    fixed code deyploy

--- vars/deployWithCompose.groovy
+++ vars/deployWithCompose.groovy
@@ -2,11 +2,22 @@ def call(String appType) {
 
     echo "Deploying using docker-compose for ${appType}"
 
+    // Load docker-compose from shared library
     def composeContent = libraryResource("${appType}/docker-compose.yml")
-
     writeFile file: 'docker-compose.yml', text: composeContent
 
+    def IMAGE_NAME = "piseth11/jenkins-react-pipeline"
+    def IMAGE_TAG  = env.BUILD_NUMBER
+    def IMAGE      = "${IMAGE_NAME}:${IMAGE_TAG}"
+
     sh """
+        docker build -t ${IMAGE} .
+        docker push ${IMAGE}
+
+        export IMAGE_NAME=${IMAGE_NAME}
+        export IMAGE_TAG=${IMAGE_TAG}
+
+        docker compose down || true
         docker compose up -d
     """
 }
