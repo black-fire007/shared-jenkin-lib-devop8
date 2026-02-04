f5621ba25b06fe45a254a2703d8b0ff56ece554a
black-fire007 <seth99991111@gmail.com>
2026-02-04 16:04:55 +0700

    fixed auto

--- vars/autoAddSubDomain.groovy
+++ vars/autoAddSubDomain.groovy
@@ -1,8 +1,17 @@
 def call(String domain, String port) {
     echo "Deploying domain ${domain} for service port ${port}"
 
-    sh """
-        chmod +x ${libraryResource('scripts/autoAddSubDomain.sh')}
-        sudo ${libraryResource('scripts/autoAddSubDomain.sh')} ${domain} ${port}
-    """
+    def scriptPath = "autoAddSubDomain.sh"
+
+    // 1. Load script from resources
+    def scriptContent = libraryResource('scripts/autoAddSubDomain.sh')
+
+    // 2. Write it to workspace
+    writeFile file: scriptPath, text: scriptContent
+
+    // 3. Make executable
+    sh "chmod +x ${scriptPath}"
+
+    // 4. Run it
+    sh "sudo ./${scriptPath} ${domain} ${port}"
 }
